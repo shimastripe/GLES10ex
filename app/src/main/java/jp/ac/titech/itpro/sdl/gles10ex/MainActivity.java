cb5f40eda23723df653be176194b37676b1423f7
Go Takagi <takagi@shimastripe.com>
2017-05-26 16:34:29 +0900

    Use accelerometer sensor

--- app/src/main/java/jp/ac/titech/itpro/sdl/gles10ex/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/gles10ex/MainActivity.java
@@ -1,18 +1,35 @@
 package jp.ac.titech.itpro.sdl.gles10ex;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.SeekBar;
 
-
-public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener, SensorEventListener {
     private final static String TAG = "MainActivity";
 
     private GLSurfaceView glView;
     private SimpleRenderer renderer;
     private SeekBar rotationBarX, rotationBarY, rotationBarZ;
+
+    private SensorManager sensorMgr;
+    private Sensor accelerometer;
+    private GraphRefreshThread th = null;
+    private Handler handler;
+    private float vx, vy, vz;
+    private float rate;
+    private int accuracy;
+    private long prevts;
+    private int progX, progY, progZ;
+
+    private final static long GRAPH_REFRESH_WAIT_MS = 20;
+    private final static float alpha = 0.75F;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +50,15 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
 //        renderer.addObj(new Pyramid(0.5f, 0, 0, 0));
         renderer.addObj(new Tetrahedron(0.5f, 0, 0, 0));
         glView.setRenderer(renderer);
+
+        sensorMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometer = sensorMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if (accelerometer == null) {
+            finish();
+            return;
+        }
+
+        handler = new Handler();
     }
 
     @Override
@@ -40,6 +66,9 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         super.onResume();
         Log.d(TAG, "onResume");
         glView.onResume();
+        sensorMgr.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+        th = new GraphRefreshThread();
+        th.start();
     }
 
     @Override
@@ -47,16 +76,24 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         super.onPause();
         Log.d(TAG, "onPause");
         glView.onPause();
+        th = null;
+        sensorMgr.unregisterListener(this);
     }
 
     @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-        if (seekBar == rotationBarX)
+        if (seekBar == rotationBarX) {
+            progX = progress;
             renderer.setRotationX(progress);
-        else if (seekBar == rotationBarY)
+        }
+        else if (seekBar == rotationBarY) {
+            progY = progress;
             renderer.setRotationY(progress);
-        else if (seekBar == rotationBarZ)
+        }
+        else if (seekBar == rotationBarZ) {
+            progZ = progress;
             renderer.setRotationZ(progress);
+        }
     }
 
     @Override
@@ -67,4 +104,39 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     public void onStopTrackingTouch(SeekBar seekBar) {
     }
 
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        Log.i(TAG, "onAccuracyChanged: ");
+        this.accuracy = accuracy;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        vx = alpha * vx + (1 - alpha) * event.values[0];
+        vy = alpha * vy + (1 - alpha) * event.values[1];
+        vz = alpha * vz + (1 - alpha) * event.values[2];
+        rate = ((float) (event.timestamp - prevts)) / (1000 * 1000);
+        prevts = event.timestamp;
+    }
+
+    private class GraphRefreshThread extends Thread {
+        public void run() {
+            try {
+                while (th != null) {
+                    handler.post(new Runnable() {
+                        public void run() {
+                            renderer.setRotationX(progX + vx*5);
+                            renderer.setRotationY(progY + vy*5);
+                            renderer.setRotationZ(progZ + vz*5);
+                        }
+                    });
+                    Thread.sleep(GRAPH_REFRESH_WAIT_MS);
+                }
+            }
+            catch (InterruptedException e) {
+                Log.e(TAG, e.toString());
+                th = null;
+            }
+        }
+    }
 }
